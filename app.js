9a3c6e4325130b14b27444aabaad76010aa820eb
thyagopacher <thyago.pacher@gmail.com>
2023-11-05 09:12:07 -0300

    get -  livros

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const livros = require('./livros');
 
 const log = (req, res, next) => {
     console.log(`................ Acesso em ${new Date()}`);
     next();
 };
+
+//identificação da rota e da const require associada
+app.use('/livros', livros);
 
 app.get('/', (req, res) => {
   res.send('Olá... Bem vindo!');
--- livros.js
+++ livros.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const router = express.Router();
+
+const dbKnex = require("./data/db_config");
+
+//método usado para consulta
+router.get("/", async (req, res) => {
+  try{
+    //para obter os livros pode usar .select().orderBy() ou apenas orderBy()
+    const livros = await dbKnex('livros').orderBy('id', 'desc');
+    res.status(200).json(livros);
+  }catch(error){
+    res.status(404).json({ msg: error.message });
+  }
+});
+
+module.exports = router;
